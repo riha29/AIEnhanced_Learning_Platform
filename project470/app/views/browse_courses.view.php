6f143cd111334084b5687a0178aeabb5b8e75456
Adiba Amreen Alam <103264221+infernora@users.noreply.github.com>
2024-04-22 23:35:25 +0600

    Update browse_courses.view.php

--- project470/app/views/browse_courses.view.php
+++ project470/app/views/browse_courses.view.php
@@ -1,12 +1,21 @@
 <?= $this->view('header');?>
 <link rel="stylesheet" href="<?= ROOT ?>/assets/css/browse_courses.css">
 <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+    
 
 <body>
 
+<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8" style="text-align:center;">
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Hello <?= $row->username?>!</h1>
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Welcome to Intellect Quest</h1>
+</div>
+ 
+<div style="text-align: center; padding: 10px 10px;"><p class="text-2xl tracking-tight text-blue-900">Embark on a linguistic adventure, challenge your friends, 
+          and climb the ranks to become an all-rounder!</p></div>
+
 <div class=searchbar>
     <form method="get" action="<?=ROOT?>/browse_courses" role="search" name="search-container">
-        <input value="<?=old_value('find','','get')?>" name="find" type="search" placeholder="Search..">
+        <input value="<?=old_value('find','','get')?>" name="find" type="search" placeholder="Search Courses...">
         <button type="submit"><i class="bi bi-search"></i></button>
     </form>
 </div>
